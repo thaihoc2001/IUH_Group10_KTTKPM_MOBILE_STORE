876c1a9b81f80f80dc98cec1f19da2a0619151aa
thaihoc2001 <thaihoc95x@gmail.com>
2022-11-22 10:19:19 +0700

    update api order

--- gateway-api-nest/src/modules/order/order.controller.ts
+++ gateway-api-nest/src/modules/order/order.controller.ts
@@ -1,15 +1,16 @@
-import { Body, Controller, Get, HttpStatus,Param,Post,Query, Req, Res, UseGuards } from "@nestjs/common";
-import { ApiBearerAuth, ApiOkResponse, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Body, CACHE_MANAGER, Controller, Get, Inject,Param,Post,Query, Req, Res } from "@nestjs/common";
+import { ApiBearerAuth, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Request, Response } from "express";
 import { createOrderRequestModel, OrderByUserFilters, OrderFilters, ResponseModel } from "src/dto";
 import { AuthService } from "../auth/auth.service";
 import { OrderService } from "./order.service";
+import { Cache } from "cache-manager";
 
 @ApiBearerAuth()
 @ApiTags('api/orders')
 @Controller('api/orders')
 export class OrderController {
-    constructor(private authService: AuthService, private orderService: OrderService){}
+    constructor(private authService: AuthService, private orderService: OrderService,  @Inject(CACHE_MANAGER) private cacheManager: Cache){}
 
     @ApiResponse({
         type: ResponseModel,
@@ -17,9 +18,16 @@ export class OrderController {
     })
     @Get('')
     async getAll(@Req() req: Request, @Res() res: Response, @Query() filters: OrderFilters) {
+        const orders = await this.cacheManager.get(
+            "order_key"
+        );
+        
+        if (orders) return res.status(200).json(orders);
+
         const token = req.headers.authorization.split(" ")[1];
         const auth = await this.authService.authentication(token);
         const data = await this.orderService.getOrders(auth.data.userId, filters);
+        this.cacheManager.set("order_key", data.data, 6000);
         return res.status(data.status).json(data.data);
     }
 
@@ -43,7 +51,13 @@ export class OrderController {
     async getAllByUser(@Req() req: Request, @Res() res: Response, @Query() filters: OrderByUserFilters) {
         const token = req.headers.authorization.split(" ")[1];
         const auth = await this.authService.authentication(token);
+        const orders = await this.cacheManager.get(
+            `order_${auth.data.userId}_key`
+        );
+        
+        if (orders) return res.status(200).json(orders);
         const data = await this.orderService.getOrdersByUser(auth.data.userId, filters);
+        this.cacheManager.set(`order_${auth.data.userId}_key`, data.data, 6000);
         return res.status(data.status).json(data.data);
     }
 
@@ -53,9 +67,15 @@ export class OrderController {
     })
     @Get(':id')
     async getById(@Req() req: Request, @Res() res: Response, @Param('id') id: string) {
+        const order = await this.cacheManager.get(
+            `order_${id}`
+        );
+        
+        if (order) return res.status(200).json(order);
         const token = req.headers.authorization.split(" ")[1];
         const auth = await this.authService.authentication(token);
         const data = await this.orderService.getOrderById(auth.data.userId, id);
+        this.cacheManager.set(`order_${id}`, data.data, 6000);
         return res.status(data.status).json(data.data);
     }
 }
