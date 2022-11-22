5c9f87586f1205ea3b5e7c5f6423d9df97932a8c
thaihoc2001 <thaihoc95x@gmail.com>
2022-11-22 08:19:19 +0700

    update api branch

--- gateway-api-nest/src/modules/brand/brand.controller.ts
+++ gateway-api-nest/src/modules/brand/brand.controller.ts
@@ -1,17 +1,18 @@
-import { Body, Controller, Get, HttpStatus,Post,Query, Req, Res, UseGuards } from "@nestjs/common";
+import { Body, CACHE_MANAGER, Controller, Get, HttpStatus,Inject,Post,Query, Req, Res, UseGuards } from "@nestjs/common";
 import { ApiBearerAuth, ApiOkResponse, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Request, Response } from "express";
 import { ResponseModel } from "src/dto";
 import { BrandFilters, CreateBrandRequestModel } from "src/dto/brand.model";
 import { AuthService } from "../auth/auth.service";
 import { BrandService } from "./brand.service";
+import { Cache } from "cache-manager";
 
 @ApiBearerAuth()
 @ApiTags('api/brands')
 @Controller('api/brands')
 export class BrandController {
 
-    constructor(private brandService: BrandService, private authService: AuthService){}
+    constructor(private brandService: BrandService, private authService: AuthService, @Inject(CACHE_MANAGER) private cacheManager: Cache){}
 
     @ApiResponse({
         type: ResponseModel,
@@ -31,7 +32,13 @@ export class BrandController {
     })
     @Get('')
     async getAll(@Req() req: Request, @Res() res: Response, @Query() filters: BrandFilters) {
+        const brands = await this.cacheManager.get(
+            "brands_key"
+        );
+        
+        if (brands) return res.status(200).json(brands);
         const data = await this.brandService.getAll(filters);
+        this.cacheManager.set("brands_key", data.data, 6000);
         return res.status(data.status).json(data.data);
     }
 
